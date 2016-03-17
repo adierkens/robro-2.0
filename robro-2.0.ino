7d5e1ec1cd6fd1dff56e587ef560c8a7cede0429
Adam Dierkens <adam@dierkens.com>
2016-03-17 18:13:03 -0400

    Fix left and right mixup

--- robro-2.0.ino
+++ robro-2.0.ino
@@ -1,10 +1,11 @@
 #include <Ultrasonic.h>
 #include <Servo.h>
 
-#define RIGHT_SERVO_PIN 9
-#define LEFT_SERVO_PIN 10
-#define RIGHT_SERVO_STOP 93
-#define LEFT_SERVO_STOP 95
+#define RIGHT_SERVO_PIN 10
+#define LEFT_SERVO_PIN 9
+
+#define RIGHT_SERVO_STOP 95
+#define LEFT_SERVO_STOP 93
 
 #define FRONT_SENSOR_TRIG_PIN 42
 #define FRONT_SENSOR_ECHO_PIN 43
@@ -21,7 +22,11 @@
 #define FOLLOWING_DISTANCE 20
 #define FOLLOWING_TOLLERANCE 5
 
-// #define SENSOR_RIGHT_SIDE
+struct distance {
+  long frontSide;
+  long backSide;
+  long front;
+};
 
 Ultrasonic front_ultrasonic(FRONT_SENSOR_TRIG_PIN,FRONT_SENSOR_ECHO_PIN);
 Ultrasonic front_side_ultrasonic(FRONT_LEFT_SENSOR_TRIG_PIN, FRONT_LEFT_SENSOR_ECHO_PIN);
@@ -37,48 +42,14 @@ void stopServos() {
 
 void backwards() {
   Serial.println("Moving backwards");
-  rightServo.write(0);
-  leftServo.write(180);
+  rightServo.write(180);
+  leftServo.write(0);
 }
 
 void forward() {
   Serial.println("Moving forward");
-  rightServo.write(180);
-  leftServo.write(0);
-}
-
-void turnRight() {
-  Serial.println("Turning right");
-  rightServo.write(180);
-  leftServo.write(180);
-}
-
-void turnLeft() {
-  Serial.println("Turning left");
-  leftServo.write(0);
   rightServo.write(0);
-}
-
-void turnSlightLeft(float distance) {
-  Serial.println("Turning slightly left");
-  float newd = distance - FOLLOWING_DISTANCE;
-  if (distance - FOLLOWING_DISTANCE > FOLLOWING_DISTANCE){
-    newd = FOLLOWING_DISTANCE;
-  }
-  long right = RIGHT_SERVO_STOP + 5 + 2 * newd;
-  long left = LEFT_SERVO_STOP - 5 - 5 * newd;
-  rightServo.write(right);
-  leftServo.write(left);
-}
-
-void turnSlightRight(float distance) {
-  Serial.println("Turning slightly right");
-  float newd = FOLLOWING_DISTANCE - distance;
-
-  float right = RIGHT_SERVO_STOP + 5 + 5 * newd;
-  float left = LEFT_SERVO_STOP - 5 - 2 * newd;
-  rightServo.write(right);
-  leftServo.write(left);
+  leftServo.write(180);
 }
 
 void setup() {
@@ -93,68 +64,62 @@ void setup() {
   digitalWrite(GND_PIN_0, LOW);
 }
 
-void printDistance(float front, float frontSide, float backSide) {
+void printDistance(distance d) {
   Serial.print("Front: ");
-  Serial.print(front);
+  Serial.print(d.front);
   Serial.print(" FrontSide: ");
-  Serial.print(frontSide);
+  Serial.print(d.frontSide);
   Serial.print(" BackSide: ");
-  Serial.println(backSide);
+  Serial.println(d.backSide);
+}
+
+distance calcDistance() {
+
+  distance calcD;
+  
+  long frontSideMicroSec = front_side_ultrasonic.timing();
+  calcD.frontSide = front_side_ultrasonic.CalcDistance(frontSideMicroSec, Ultrasonic::CM);
+  delay(2);
+  long backSideMicroSec = back_side_ultrasonic.timing();
+  calcD.backSide = back_side_ultrasonic.CalcDistance(backSideMicroSec, Ultrasonic::CM);
+
+  long frontMicroSec = front_ultrasonic.timing();
+  calcD.front = front_ultrasonic.CalcDistance(frontMicroSec, Ultrasonic::CM);
+
+  printDistance(calcD);
+
+  return calcD;  
+}
+
+bool run = false;
+
+void handleOutsideCorner(distance d) {
+  Serial.println("Hit outside corner");
+
+  // Initiate the left turn
+  float right = 0;
+  float left = LEFT_SERVO_STOP;
+  
+  leftServo.write(left);
+  rightServo.write(right);
+  delay(800);
+
+  stopServos();
+  run = true;
 }
 
 void loop() {
 
-  long frontSideMicroSec = front_side_ultrasonic.timing();
-  float frontSideDistance = front_side_ultrasonic.CalcDistance(frontSideMicroSec, Ultrasonic::CM);
-  delay(5);
-  long backSideMicroSec = back_side_ultrasonic.timing();
-  float backSideDistance = back_side_ultrasonic.CalcDistance(backSideMicroSec, Ultrasonic::CM);
+  distance d = calcDistance();
 
-  long frontMicroSec = front_ultrasonic.timing();
-  float frontDistance = front_ultrasonic.CalcDistance(frontMicroSec, Ultrasonic::CM);
+  if (run) {
+    return;
+  }
 
-  printDistance(frontDistance, frontSideDistance, backSideDistance);
-
-  float avgDistance = (frontSideDistance + backSideDistance) / 2;
-
-
-  if (frontDistance < FOLLOWING_DISTANCE) {
-    turnRight();
+  // Follow wall
+  if ((d.frontSide - d.backSide) > (2 * FOLLOWING_DISTANCE)) {
+    handleOutsideCorner(d);
   } else {
-    if (frontSideDistance < FOLLOWING_DISTANCE) {
-      bool frontSideWithinTollerance = frontSideDistance > (FOLLOWING_DISTANCE + FOLLOWING_TOLLERANCE);
-
-      if (backSideDistance < FOLLOWING_DISTANCE) {
-
-        
-        bool backSideWithinTollerance = backSideDistance > (FOLLOWING_DISTANCE + FOLLOWING_TOLLERANCE);
-
-        if (!frontSideWithinTollerance || !backSideWithinTollerance) {
-
-          if (frontSideWithinTollerance) {
-            forward();
-          } else if (backSideWithinTollerance) {
-            turnSlightRight(avgDistance);
-          } else {
-            turnSlightRight(avgDistance);
-          }
-          
-        } else {
-          forward();
-        }
-      } else {
-        if (!frontSideWithinTollerance) {
-          turnSlightRight(avgDistance);
-        } else {
-          forward();
-        }
-      }
-    } else {
-      if (backSideDistance < FOLLOWING_DISTANCE) {
-        turnLeft();
-      } else {
-        forward();
-      }
-    }
-  }  
+    forward();
+  }
 }
