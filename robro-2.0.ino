50ee43a2e28b8e6fa2cbe6b67513891af27016f1
Dan Jamison <djamison05@gmail.com>
2016-04-06 19:34:40 -0400

    first rev ronaldbro_2.0

--- robro-2.0.ino
+++ robro-2.0.ino
@@ -1,11 +1,16 @@
 #include <QTRSensors.h>
 #include <Servo.h>
+#include <Ultrasonic.h>
 
 #define RIGHT_SERVO_PIN 10
 #define LEFT_SERVO_PIN 9
 
 #define RIGHT_SERVO_STOP 95
 #define LEFT_SERVO_STOP 93
+
+#define SENSOR_TRIG_PIN 44
+#define SENSOR_ECHO_PIN 45
+#define TRIGGER_PIN 53
 
 #define NUM_SENSORS   8     // number of sensors used
 #define TIMEOUT       2500  // waits for 2500 microseconds for sensor outputs to go low
@@ -19,13 +24,19 @@
 #define KP 0.01 * (M_START - 1)
 #define KD 0.01
 
+#define SENSOR_THRESHOLD 300
+#define DIST_THRESHOLD 30
+
+
 const int calibratedMax[NUM_SENSORS] = {2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500};
 const int calibratedMin[NUM_SENSORS] = { 348,  348, 296, 248, 252, 300, 300, 456 };
 int lastError = 0;
+Ultrasonic ultrasonic(SENSOR_TRIG_PIN,SENSOR_ECHO_PIN);
 Servo rightServo;
 Servo leftServo;
-QTRSensorsRC qtrc((unsigned char[]) { 30, 31, 32, 33, 34, 35, 36, 37}, NUM_SENSORS, TIMEOUT, EMITTER_PIN); 
+QTRSensorsRC qtrc((unsigned char[]) { 22, 23, 24, 25, 26, 27, 28, 29}, NUM_SENSORS, TIMEOUT, EMITTER_PIN); 
 unsigned int sensorValues[NUM_SENSORS];
+boolean ballFound = false;
 
 void setup() {
   Serial.begin(9600);
@@ -55,7 +66,37 @@ int readSensorValues() {
   return position;
 }
 
-void loop() {
+void forward(){
+  Serial.printIn("Moving forward");
+  rightServo.write(0);
+  leftServo.write(180);
+}
+
+void backward(){
+  Serial.printIn("Moving backward");
+  rightServo.write(180);
+  leftServo.write(0);
+}
+
+void turnRight(){
+  Serial.printIn("Turning right");
+  do{
+    rightServo.write(0);
+    leftServo.write(0);
+
+    readSensorValues();
+  }while(sensorValues[4] > SENSOR_THRESHOLD && sensorValues[5] < SENSOR_THRESHOLD);
+}
+
+
+void turnLeft(){
+  Serial.printIn("Turning left");
+    rightServo.write(RIGHT_SERVO_STOP);
+    leftServo.write(180);
+    delay(700);
+}
+
+void trackLine() {
   int position = readSensorValues();  
   int error = position - (1000 * (NUM_SENSORS - 1))/2;
   int motorSpeed = KP * error + KD * (error - lastError);
@@ -67,4 +108,49 @@ void loop() {
   rightServo.write(rightSpeed);
   leftServo.write(leftSpeed);
   delay(2);
+}
+
+void startRun(){
+  do{
+      forward();
+      int numSensorsOn = 0;  
+      readSensorValues();
+      for (unsigned char i = 0; i < NUM_SENSORS; i++) {
+        if (sensorValues[i] > SENSOR_THRESHOLD){
+          numSensorsOn++
+          }
+  }while(numSensorsOn != NUM_SENSORS);
+}
+
+void printDistance(long d){
+  Serial.print("Distance: ");
+  Serial.print(d);
+}
+
+long calcDistance(){
+  int calcD;
+  long frontMicroSec = ultrasonic.timing();
+  calcD = ultrasonic.CalcDistance(frontMicroSec, Ultrasonic::CM);
+  printDistance(calcD); 
+  
+  return calcD;
+}
+
+void loop() {
+  startRun();
+
+  turnRight();
+
+  do{
+    trackLine();
+    long d = calcDistance();
+    if (d < DIST_THRESHOLD){
+      ballFound = true;
+      }
+    }while(!ballFound)
+  
+  turnLeft();
+
+
+
 }
